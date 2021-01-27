2b0d9da45fd013512dc725d09d95812a1c626923
nahid hasan <nahidhasan.citl@gmail.com>
2021-01-27 18:39:26 +0600

    heroku app deployed

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="m-auto">
-<h1 class="text-2xl text-center">Welcome to 10 Apps created with Vue</h1>
+<h1 class="text-2xl text-center">Welcome to 10 Apps created with Vue, Pushed to heroku</h1>
 </div>
 </template>
 
