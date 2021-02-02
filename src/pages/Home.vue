8c6a41af8a24e6aa05de8cf4b988638f5da7e004
nahid hasan <nahidhasan.citl@gmail.com>
2021-02-02 15:09:09 +0600

    done

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,59 @@
 <template>
-
 <div class="container">
+  <div class="profile-card">
+    <div class="card-header">
+      <img class="profile-image" src="https://static.wixstatic.com/media/f1c3e8_1fd67a6c77014de784ff69620189479c~mv2_d_1448_2048_s_2.jpg/v1/fill/w_470,h_470,al_c,q_80,usm_0.66_1.00_0.01/f1c3e8_1fd67a6c77014de784ff69620189479c~mv2_d_1448_2048_s_2.webp" alt="profile image">
+      <div class="profile-name">Nahid Hasan</div>
+      <div class="profile-role">Full Stack Developer</div>
+      <div class="profile-location flex">
+        <svg class="icon white" viewBox="0 0 20 20">
+          <path d="M10,1.375c-3.17,0-5.75,2.548-5.75,5.682c0,6.685,5.259,11.276,5.483,11.469c0.152,0.132,0.382,0.132,0.534,0c0.224-0.193,5.481-4.784,5.483-11.469C15.75,3.923,13.171,1.375,10,1.375 M10,17.653c-1.064-1.024-4.929-5.127-4.929-10.596c0-2.68,2.212-4.861,4.929-4.861s4.929,2.181,4.929,4.861C14.927,12.518,11.063,16.627,10,17.653 M10,3.839c-1.815,0-3.286,1.47-3.286,3.286s1.47,3.286,3.286,3.286s3.286-1.47,3.286-3.286S11.815,3.839,10,3.839 M10,9.589c-1.359,0-2.464-1.105-2.464-2.464S8.641,4.661,10,4.661s2.464,1.105,2.464,2.464S11.359,9.589,10,9.589">
+          </path>
+        </svg>
+        Dhaka, Bangladesh
+      </div>
+      <div class="divider">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"></path>
+        </svg>
+      </div>
+    </div>
+    <div class="card-body">
+      <div class="contacts">
+        <div class="title">Contacts</div>
+        <div class="contact-list">
+          <a class="contact-item facebook" href="#">
+            <svg class="contact-icon" viewBox="0 0 20 20">
+              <path fill="none" d="M11.344,5.71c0-0.73,0.074-1.122,1.199-1.122h1.502V1.871h-2.404c-2.886,0-3.903,1.36-3.903,3.646v1.765h-1.8V10h1.8v8.128h3.601V10h2.403l0.32-2.718h-2.724L11.344,5.71z"></path>
+            </svg>
+          </a>
+
+          <a class="contact-item twitter" href="#">
+            <svg class="contact-icon" viewBox="0 0 20 20">
+              <path fill="none" d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"></path>
+            </svg>
+          </a>
+
+          <a class="contact-item instagram" href="#">
+            <svg class="contact-icon" viewBox="0 0 20 20">
+              <path fill="none" d="M14.52,2.469H5.482c-1.664,0-3.013,1.349-3.013,3.013v9.038c0,1.662,1.349,3.012,3.013,3.012h9.038c1.662,0,3.012-1.35,3.012-3.012V5.482C17.531,3.818,16.182,2.469,14.52,2.469 M13.012,4.729h2.26v2.259h-2.26V4.729z M10,6.988c1.664,0,3.012,1.349,3.012,3.012c0,1.664-1.348,3.013-3.012,3.013c-1.664,0-3.012-1.349-3.012-3.013C6.988,8.336,8.336,6.988,10,6.988 M16.025,14.52c0,0.831-0.676,1.506-1.506,1.506H5.482c-0.831,0-1.507-0.675-1.507-1.506V9.247h1.583C5.516,9.494,5.482,9.743,5.482,10c0,2.497,2.023,4.52,4.518,4.52c2.494,0,4.52-2.022,4.52-4.52c0-0.257-0.035-0.506-0.076-0.753h1.582V14.52z"></path>
+            </svg>
+          </a>
+
+          <a class="contact-item dribbble" href="#">
+            <svg class="contact-icon" viewBox="0 0 20 20">
+							<path fill="none" d="M9.426,7.625h0.271c0.596,0,1.079-0.48,1.079-1.073V4.808c0-0.593-0.483-1.073-1.079-1.073H9.426c-0.597,0-1.079,0.48-1.079,1.073v1.745C8.347,7.145,8.83,7.625,9.426,7.625 M9.156,4.741c0-0.222,0.182-0.402,0.404-0.402c0.225,0,0.405,0.18,0.405,0.402V6.62c0,0.222-0.181,0.402-0.405,0.402c-0.223,0-0.404-0.181-0.404-0.402V4.741z M12.126,7.625c0.539,0,1.013-0.47,1.013-0.47v0.403h0.81V3.735h-0.81v2.952c0,0-0.271,0.335-0.54,0.335c-0.271,0-0.271-0.202-0.271-0.202V3.735h-0.81v3.354C11.519,7.089,11.586,7.625,12.126,7.625 M6.254,7.559H7.2v-2.08l1.079-2.952H7.401L6.727,4.473L6.052,2.527H5.107l1.146,2.952V7.559z M11.586,12.003c-0.175,0-0.312,0.104-0.405,0.204v2.706c0.086,0.091,0.213,0.18,0.405,0.18c0.405,0,0.405-0.451,0.405-0.451v-2.188C11.991,12.453,11.924,12.003,11.586,12.003 M14.961,8.463c0,0-2.477-0.129-4.961-0.129c-2.475,0-4.96,0.129-4.96,0.129c-1.119,0-2.025,0.864-2.025,1.93c0,0-0.203,1.252-0.203,2.511c0,1.252,0.203,2.51,0.203,2.51c0,1.066,0.906,1.931,2.025,1.931c0,0,2.438,0.129,4.96,0.129c2.437,0,4.961-0.129,4.961-0.129c1.117,0,2.024-0.864,2.024-1.931c0,0,0.202-1.268,0.202-2.51c0-1.268-0.202-2.511-0.202-2.511C16.985,9.328,16.078,8.463,14.961,8.463 M7.065,10.651H6.052v5.085H5.107v-5.085H4.095V9.814h2.97V10.651z M9.628,15.736h-0.81v-0.386c0,0-0.472,0.45-1.012,0.45c-0.54,0-0.606-0.515-0.606-0.515v-3.991h0.809v3.733c0,0,0,0.193,0.271,0.193c0.27,0,0.54-0.322,0.54-0.322v-3.604h0.81V15.736z M12.801,14.771c0,0,0,1.03-0.742,1.03c-0.455,0-0.73-0.241-0.878-0.429v0.364h-0.876V9.814h0.876v1.92c0.135-0.142,0.464-0.439,0.878-0.439c0.54,0,0.742,0.45,0.742,1.03V14.771z M15.973,12.39v1.287h-1.688v0.965c0,0,0,0.451,0.405,0.451s0.405-0.451,0.405-0.451v-0.45h0.877v0.708c0,0-0.136,0.901-1.215,0.901c-1.08,0-1.282-0.901-1.282-0.901v-2.51c0,0,0-1.095,1.282-1.095S15.973,12.39,15.973,12.39 M14.69,12.003c-0.405,0-0.405,0.45-0.405,0.45v0.579h0.811v-0.579C15.096,12.453,15.096,12.003,14.69,12.003"></path>
+						</svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- <div class="container">
   <div class="profile-card">
     <div class="profile-card__header">
       <div class="profile-card__image">
@@ -15,19 +68,12 @@
   <a href="https://codepen.io/EdgarElias/" target="_blank"><img class="social" src="https://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-Black-Large.png" alt="social"/></a>
         </div>
   </div>
-</div>
+</div> -->
 <!-- <div class="m-auto">
 <h1 class="text-2xl text-center">Welcome to 10 Apps created with Vue by Nahid !!</h1>
 </div> -->
 </template>
 <script>
-window.onload = function() {
-	setTimeout(function() {
-		document.querySelector(".profile-card").classList.add("active");
-	}, 400);
-};
-
-
 export default {
 
 }
@@ -36,193 +82,128 @@ export default {
 <style>
 
 @import url("https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i");
-
-/* body {
-  background-color: #94553D;
-  font-family: "Open Sans", Arial, sans-serif;
-} */
-
-img.social{
-height:40px;
-width: 40px;
-padding:2px;
-margin:10px;
-}
-
 .container {
-  position: fixed;
-  top: 40px;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
   display: flex;
+  min-height: 100vh;
   align-items: center;
   justify-content: center;
-  perspective: 500px;
+  padding: 2rem;
 }
 
 .profile-card {
-  width: 400px;
-  overflow: hidden;
-  border-radius: 4px;
-  box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.5);
-  transform-origin: 50%, 20%;
-  transform-style: preserve-3d;
-  -webkit-animation: profileCardAnimate 0.5s ease forwards;
-          animation: profileCardAnimate 0.5s ease forwards;
-}
-.profile-card.active .profile-card__image .wrapper {
-  -webkit-animation: profileImageWrapper 0.5s ease forwards;
-          animation: profileImageWrapper 0.5s ease forwards;
-}
-.profile-card.active .profile-card__info span:nth-child(1) {
-  -webkit-animation: profileInfoSpan 0.5s ease forwards;
-          animation: profileInfoSpan 0.5s ease forwards;
-  -webkit-animation-delay: 0.1s;
-          animation-delay: 0.1s;
-}
-.profile-card.active .profile-card__info span:nth-child(2) {
-  -webkit-animation: profileInfoSpan 0.5s ease forwards;
-          animation: profileInfoSpan 0.5s ease forwards;
-  -webkit-animation-delay: 0.2s;
-          animation-delay: 0.2s;
-}
-.profile-card.active .profile-card__footer span:nth-child(1) {
-  -webkit-animation: profileFooterSpan 0.5s ease forwards;
-          animation: profileFooterSpan 0.5s ease forwards;
-  -webkit-animation-delay: 0.1s;
-          animation-delay: 0.1s;
-}
-.profile-card.active .profile-card__footer span:nth-child(2) {
-  -webkit-animation: profileFooterSpan 0.5s ease forwards;
-          animation: profileFooterSpan 0.5s ease forwards;
-  -webkit-animation-delay: 0.2s;
-          animation-delay: 0.2s;
-}
-.profile-card.active .profile-card__footer span:nth-child(3) {
-  -webkit-animation: profileFooterSpan 0.5s ease forwards;
-          animation: profileFooterSpan 0.5s ease forwards;
-  -webkit-animation-delay: 0.3s;
-          animation-delay: 0.3s;
-}
-.profile-card__header {
-  display: flex;
-  align-items: center;
-  padding: 1.5rem 2rem;
-  background-color: #fff;
-  overflow: hidden;
-}
-.profile-card__image {
-  width: 150px;
-  height: 150px;
-  margin-right: 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-}
-.profile-card__image .wrapper {
   position: relative;
-  width: 0;
-  height: 0;
+  width: 500px;
+  background-color: #FFF;
+  border-radius: 0.5rem;
   overflow: hidden;
+  transition: all 0.25s ease;
 }
-.profile-card__image .wrapper img {
-  max-width: 100%;
-  max-width: 100%;
-  border-radius: 500px;
-  border: 0.3rem solid #cccccc;
-  box-sizing: border-box;
+.profile-card:hover {
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.15);
 }
-.profile-card__info {
+.profile-card .card-header {
+  position: relative;
   display: flex;
-  flex-direction: column;
-  align-content: center;
-  color: #b3b3b3;
-}
-.profile-card__name, .profile-card__designation {
-  align-self: flex-start;
-  transform: translateX(500%);
-}
-.profile-card__name {
-  font-size: 1.5rem;
-  margin-bottom: 0.2rem;
-}
-.profile-card__designation {
-  font-size: 0.9rem;
-}
-.profile-card__footer {
-  display: flex;
-  justify-content: center;
-  background-color: white;
-}
-.profile-card__footer span {
-  padding: 2rem;
-  display: inline-flex;
-  flex-direction: column;
   align-items: center;
-  color: #b3b3b3;
-  font-size: 1.2rem;
-  transform: translateY(100%);
+  justify-content: center;
+  flex-direction: column;
+  padding: 2rem;
+  color: #FFF;
+  background: linear-gradient(135deg, #3f5efb, #fc466b);
 }
-.profile-card__footer span i {
-  margin-bottom: 0.5rem;
+.profile-card .card-header .profile-image {
+  width: 160px;
+  height: 160px;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 5px solid #FFF;
 }
-.profile-card__footer span:hover {
-  color: #333333;
-  cursor: pointer;
+.profile-card .card-header .profile-name {
+  margin-top: 0.5rem;
+  font-size: 2rem;
+  font-weight: 700;
+}
+.profile-card .card-header .profile-role {
+  font-size: 1.05rem;
+  color: #DDD;
+}
+.profile-card .card-header .profile-location {
+  margin-top: 0.5rem;
+  margin-bottom: 2rem;
+  font-size: 1.25rem;
+}
+.profile-card .card-header .divider {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+  transform: rotate(180deg);
+}
+.profile-card .card-header .divider svg {
+  position: relative;
+  display: block;
+  width: calc(150% + 1.5px);
+  height: 50px;
+}
+.profile-card .card-header .divider svg path {
+  fill: #FFFFFF;
+}
+.profile-card .card-body {
+  padding: 2rem;
+}
+.profile-card .card-body .title {
+  color: #666;
+  margin-bottom: 1rem;
+  text-align: center;
+  font-size: 1.05rem;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+.profile-card .card-body .contact-list {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.profile-card .card-body .contact-list .contact-item {
+  width: 50px;
+  height: 50px;
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+}
+.profile-card .card-body .contact-list .contact-item.facebook {
+  background: linear-gradient(135deg, #00c6ff, #0072ff);
+  box-shadow: 0px 5px 10px rgba(0, 198, 255, 0.5);
+}
+.profile-card .card-body .contact-list .contact-item.twitter {
+  background: linear-gradient(135deg, #00d2ff, #3a7bd5);
+  box-shadow: 0px 5px 10px rgba(0, 210, 255, 0.5);
+}
+.profile-card .card-body .contact-list .contact-item.instagram {
+  background: linear-gradient(135deg, #833ab4, #fd1d1d, #fcb045);
+  box-shadow: 0px 5px 10px rgba(253, 29, 29, 0.5);
+}
+.profile-card .card-body .contact-list .contact-item.dribbble {
+  background: linear-gradient(135deg, #da4453, #89216b);
+  box-shadow: 0px 5px 10px rgba(218, 68, 83, 0.5);
 }
 
-@-webkit-keyframes profileImageWrapper {
-  100% {
-    width: 120px;
-    height: 120px;
-  }
+.icon {
+  position: relative;
+  top: 0.25rem;
+  width: 1.25em;
+  height: 1.25em;
+}
+.icon.white path, .icon.white polygon, .icon.white rect {
+  fill: #FFF;
 }
 
-@keyframes profileImageWrapper {
-  100% {
-    width: 120px;
-    height: 120px;
-  }
+.contact-icon {
+  width: 100%;
+  height: 100%;
 }
-@-webkit-keyframes profileInfoSpan {
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
-}
-@keyframes profileInfoSpan {
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
-}
-@-webkit-keyframes profileFooterSpan {
-  100% {
-    transform: translateY(0);
-  }
-}
-@keyframes profileFooterSpan {
-  100% {
-    transform: translateY(0);
-  }
-}
-@-webkit-keyframes profileCardAnimate {
-  0%, 25% {
-    transform: translateZ(-400px) rotateX(-90deg);
-  }
-  100% {
-    transform: translateZ(0) rotateX(0);
-  }
-}
-@keyframes profileCardAnimate {
-  0%, 25% {
-    transform: translateZ(-400px) rotateX(-90deg);
-  }
-  100% {
-    transform: translateZ(0) rotateX(0);
-  }
+.contact-icon path {
+  fill: #FFF;
 }
 </style>
